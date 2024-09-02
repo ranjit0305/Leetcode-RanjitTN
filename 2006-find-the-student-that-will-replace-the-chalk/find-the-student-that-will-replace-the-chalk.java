039beb9eff0530108b101710b233ed3b53605a52
Ranjit T N <ranju0305@gmail.com>
2024-09-02 19:58:42 +0530

    Time: 1790 ms (16.86%) | Memory: 60.6 MB (6.23%) - LeetSync

---
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int chalkReplacer(int[] chalk, int k) 
+    {
+        int i=0;
+        while(k>=chalk[i])
+        {
+            k=k-chalk[i];
+            i++;
+            if(i>=chalk.length)
+            {
+                i=0;
+            }
+        }
+    return i;
+    }
+}
