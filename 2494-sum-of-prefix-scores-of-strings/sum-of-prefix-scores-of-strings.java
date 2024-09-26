517e0869c5d4c471b14cd9d1e927958170a7efb5
Ranjit T N <ranju0305@gmail.com>
2024-09-26 22:45:53 +0530

    Time: 340 ms (29.87%) | Memory: 187 MB (85.06%) - LeetSync

--- 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.java
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.java
@@ -1,70 +1,66 @@
 class Node {
-    Map<Character, Pair> umc = new HashMap<>();
-    boolean isEnd = false;
-}
+    int count = 0;
+    Node[] list = new Node[26];
 
-class Pair {
-    int count;
-    Node node;
-
-    Pair(int count, Node node) {
-        this.count = count;
-        this.node = node;
-    }
-}
-
-class Trie {
-    private Node root;
-
-    public Trie() {
-        root = new Node();
+    public boolean containKey(char ch) {
+        return list[ch - 'a'] != null;
     }
 
-    // Insert word into the Trie
-    public void insert(String s) {
-        Node cur = root;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!cur.umc.containsKey(c)) {
-                cur.umc.put(c, new Pair(0, new Node()));
-            }
-            cur.umc.get(c).count++;
-            cur = cur.umc.get(c).node;
-        }
-        cur.isEnd = true;
+    public Node get(char ch) {
+        return list[ch - 'a'];
     }
 
-    // Check prefix count of the word
-    public int checkPrefixCount(String s) {
-        Node cur = root;
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!cur.umc.containsKey(c)) {
-                break;
-            }
-            count += cur.umc.get(c).count;
-            cur = cur.umc.get(c).node;
-        }
-        return count;
+    public void put(char ch, Node new_node) {
+        list[ch - 'a'] = new_node;
+    }
+
+    public void inc(char ch) {
+        list[ch - 'a'].count++;
+    }
+
+    public int retCount(char ch) {
+        return list[ch - 'a'].count;
     }
 }
 
 class Solution {
+    private Node root;
+
+    public Solution() {
+        root = new Node();
+    }
+
+    public void insert(String word) {
+        Node node = root;
+        for (char ch : word.toCharArray()) {
+            if (!node.containKey(ch)) {
+                node.put(ch, new Node());
+            }
+            node.inc(ch);
+            node = node.get(ch);
+        }
+    }
+
+    public int search(String word) {
+        Node node = root;
+        int preCount = 0;
+        for (char ch : word.toCharArray()) {
+            preCount += node.retCount(ch);
+            node = node.get(ch);
+        }
+        return preCount;
+    }
+
     public int[] sumPrefixScores(String[] words) {
-        Trie trie = new Trie();
-        
-        // Insert all words into the Trie
+        // This problem can be solved using the trie data structure
         for (String word : words) {
-            trie.insert(word);
+            insert(word);
         }
-
-        // Collect the prefix scores for each word
-        int[] ans = new int[words.length];
-        for (int i = 0; i < words.length; i++) {
-            ans[i] = trie.checkPrefixCount(words[i]);
+        int n = words.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = search(words[i]);
         }
-
-        return ans;
+        return res;
     }
 }
