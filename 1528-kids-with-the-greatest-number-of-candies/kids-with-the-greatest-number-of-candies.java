53621207877d3213e850d930e173cb2aad710377
Ranjit T N <ranju0305@gmail.com>
2024-09-29 22:42:53 +0530

    Time: 1 ms (86.93%) | Memory: 42.3 MB (40.91%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -1,24 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        List<Boolean> arr = new ArrayList<>();  // Fixed: Use Boolean instead of boolean
-        for (int i = 0; i < candies.length; i++) {
-            int a = candies[i] + extraCandies;  // Add extra candies
-            boolean res = true;  // Assume true initially
-            for (int j = 0; j < candies.length; j++) {  // Fixed: Changed 'i' to 'j'
-                if (!checkMax(a, candies[j])) {
-                    res = false;  // If any kid has more candies than 'a', set to false
-                    break;  // No need to check further
-                }
+        List<Boolean>list=new ArrayList<>();
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<candies.length;i++)
+        {
+            if(candies[i]>max)
+            {
+                max=candies[i];
             }
-            arr.add(res);  // Fixed: Use 'arr' to add the result
         }
-        return arr;  // Return the final list
-    }
-
-    public boolean checkMax(int ele1, int ele2) {
-        return ele1 >= ele2;  // Compare the two elements
+        for(int i=0;i<candies.length;i++)
+        {
+            if(candies[i]+extraCandies>=max)
+            {
+                list.add(true);
+            }
+            else
+            {
+                list.add(false);
+            }
+        }
+      return list;  
     }
 }
