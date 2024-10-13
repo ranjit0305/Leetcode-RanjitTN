9127d593230a43d6000b4a885ed1e30802a47e71
Ranjit T N <ranju0305@gmail.com>
2024-10-13 15:17:48 +0530

    Time: 2 ms (85.54%) | Memory: 45.8 MB (66.60%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] num) {
+        int j = 0, t;
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] != 0) {
+                t = num[i];
+                num[i] = num[j];
+                num[j] = t;
+                j++;
+            }
+        }
+    }
+
+}
