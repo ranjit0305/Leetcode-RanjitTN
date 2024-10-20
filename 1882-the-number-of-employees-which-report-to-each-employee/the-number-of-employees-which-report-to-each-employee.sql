cabeaec4ce4a02f278a73c60b36ba2d07147f095
Ranjit T N <ranju0305@gmail.com>
2024-10-20 21:12:26 +0530

    Time: 562 ms (78.03%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,5 +1,7 @@
-/* Write your T-SQL query statement below */
-select e1.employee_id,e1.name,count(e2.reports_to) reports_count,round(avg(e2.age*1.00),0) average_age
-from employees e1 inner join employees e2 on e2.reports_to=e1.employee_id
-group by e1.employee_id,e1.name
-order by e1.employee_id
+select mgr.employee_id, mgr.name, COUNT(emp.employee_id) as reports_count, ROUND(AVG(emp.age)) as average_age
+from employees emp join employees mgr
+on emp.reports_to = mgr.employee_id
+group by employee_id
+order by employee_id
+
+# Do upvote if you like the solution
