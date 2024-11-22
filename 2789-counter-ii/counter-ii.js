879aa7cda90794f8df6e1e4b59d95840700ee187
Ranjit T N <ranju0305@gmail.com>
2024-11-22 21:58:48 +0530

    Time: 64 ms (28.91%) | Memory: 51.5 MB (66.50%) - LeetSync

--- 2789-counter-ii/counter-ii.js
+++ 2789-counter-ii/counter-ii.js
@@ -12,7 +12,36 @@ var createCounter = function (init) {
     },
     reset: function () {
       val = init;
-      return val;
+      return val;/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let num = init;
+
+
+    return {
+        increment: () => {
+            num++;
+            return num;
+        },
+        reset: () => {
+            num = init;
+            return num;
+        },
+        decrement: () => {
+            num--;
+            return num;
+        }
+    }    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
     },
     decrement: function () {
       val -= 1;
