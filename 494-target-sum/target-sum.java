5423cd444ee3bc61dcf0561257d60a1b5a678108
Ranjit T N <ranju0305@gmail.com>
2024-12-26 22:37:33 +0530

    Time: 1 ms (100.00%) | Memory: 40.6 MB (99.57%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,21 +1,33 @@
 class Solution {
-    private int[] arr;
-    
-    private void run(int id, int target, int[] c, int sum) {
-        if (id == arr.length) {
-            if (target == sum) {
-                c[0]++;
-            }
+    int[] dp;
+    public int findTargetSumWays(int[] nums, int target) {
+        // use DP
+        // assume that we have S1 - S2 = target && S1 + S2 = sum ==> S1 = (target + sum)/2
+        // it means that we need to find the number of subsets that have sum = (target + sum)/2
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (target > sum || target + sum < 0 || (target+sum)%2 != 0) {
+            return 0;
+        }
+        int targetSum = (target + sum)/2;
+        // we use DP to represents the number of subset that have sum equals targetSum
+        dp = new int[targetSum+1]; 
+        dp[0] = 1; // we have at least one way to have sum = 0 as empty subset
+
+        countSubsets(nums, 0, targetSum);
+        return dp[targetSum];
+    }
+
+    private void countSubsets(int[] nums, int index, int targetSum) {
+        if (index == nums.length) {
             return;
         }
-        run(id + 1, target, c, sum - arr[id]);
-        run(id + 1, target, c, sum + arr[id]);
-    }
-    
-    public int findTargetSumWays(int[] nums, int target) {
-        arr = nums;
-        int[] c = new int[1];
-        run(0, target, c, 0);
-        return c[0];
+        
+        for (int j=targetSum; j>=nums[index]; j--) {
+            dp[j] += dp[j-nums[index]];
+        }
+        countSubsets(nums, index+1, targetSum);
     }
 }
