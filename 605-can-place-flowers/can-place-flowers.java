4b21fa010f97b235bdfc44515b2c26ee81004ad3
Ranjit T N <ranju0305@gmail.com>
2025-02-07 09:09:29 -0800

    Time: 1 ms (97.18%) | Memory: 45.5 MB (87.21%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -3,9 +3,7 @@ class Solution {
         int cnt = n;
         for (int i = 0; i < flowerbed.length; i++) {
             if (flowerbed[i] == 0 && 
-                (i == 0 || flowerbed[i - 1] == 0) &&  // Check left boundary
-                (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) { // Check right boundary
-                
+                (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
                 flowerbed[i] = 1;
                 cnt--;
             }
