1c9abef92c36a140a3b331340dcef7523a760040
Ranjit T N <ranju0305@gmail.com>
2025-02-08 09:23:13 -0800

    Time: 1 ms (66.09%) | Memory: 45.6 MB (6.58%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int min = nums[0], max = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < min) min = nums[i];
+            if (nums[i] > max) max = nums[i];
+        }
+
+        int[] freq = new int[max - min + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            freq[nums[i] - min]++;
+        }
+
+        int index = 0;
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] > 0) {
+                nums[index++] = i + min;
+            }
+        }
+
+        return index;
+    }
+}
