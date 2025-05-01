8c80a5c99f34d44fd227ed7d4abf2de4dcc8012e
Ranjit T N <ranju0305@gmail.com>
2025-05-01 22:03:07 +0530

    Time: 23 ms (5.84%) | Memory: 40.7 MB (83.97%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int mySqrt(int n) {
+        int ans = 0;
+        for (long i = 1; i <= n; i++) {
+            long val = i * i;
+            if (val <= (long) n) {
+                ans = (int) i;
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+}
