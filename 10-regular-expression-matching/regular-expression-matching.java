975976a002b15ac1e3863cfd4dafefd5f04c77fc
Ranjit T N <ranju0305@gmail.com>
2025-05-28 19:41:43 +0530

    Time: 964 ms (8.85%) | Memory: 45.1 MB (9.71%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,5 +1,26 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        return s.matches("^" + p + "$");
+        if (p.length() == 0) {
+            return s.length() == 0;
+        }
+
+        if (p.length() > 1 && p.charAt(1) == '*') {
+            // Case 1: Remove char and '*' in pattern
+            if (isMatch(s, p.substring(2))) {
+                return true;
+            }
+            // Case 2: Use '*' to match one or more characters
+            if (s.length() > 0 && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0))) {
+                return isMatch(s.substring(1), p);
+            }
+            return false;
+        } else {
+            // No '*', check current char and move forward
+            if (s.length() > 0 && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0))) {
+                return isMatch(s.substring(1), p.substring(1));
+            }
+        }
+
+        return false;
     }
 }
