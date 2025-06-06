5ddac22b0a8387ff8cc0ba551990d4a19a7d0fc1
Ranjit T N <ranju0305@gmail.com>
2025-06-06 08:49:44 +0530

    Time: 183 ms (20.28%) | Memory: 55.5 MB (55.94%) - LeetSync

---
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String robotWithString(String s) {
+        int n = s.length();
+        char[] rightMin = new char[n];
+        rightMin[n - 1] = s.charAt(n - 1);
+        for (int i = n - 2; i >= 0; i--)
+            rightMin[i] = s.charAt(i) < rightMin[i + 1] ? s.charAt(i) : rightMin[i + 1];
+        StringBuilder ans = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            char curr = s.charAt(i);
+            while (!stack.isEmpty() && stack.peek() <= curr && rightMin[i] >= stack.peek())
+                ans.append(stack.pop());
+
+            stack.push(curr);
+        }
+        while(!stack.isEmpty())
+            ans.append(stack.pop());
+        
+        return ans.toString();
+
+    }
+}
