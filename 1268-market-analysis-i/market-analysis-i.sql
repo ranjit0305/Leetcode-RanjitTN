2694e865ba54a401138bcbd389ac4c93a58444e6
Ranjit T N <ranju0305@gmail.com>
2025-06-14 16:26:02 +0530

    Time: 1200 ms (71.65%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -0,0 +1,1 @@
+SELECT u.user_id AS buyer_id,u.join_date,COUNT(o.order_id) AS orders_in_2019 FROM users u LEFT JOIN orders o ON u.user_id = o.buyer_id AND o.order_date BETWEEN '2019-01-01' AND '2019-12-31' GROUP BY u.user_id, u.join_date;
