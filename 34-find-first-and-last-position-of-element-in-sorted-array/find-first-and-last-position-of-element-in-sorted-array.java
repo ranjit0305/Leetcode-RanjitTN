8347268d95e7880ac6827d601a45cd4cb22379c8
Ranjit T N <ranju0305@gmail.com>
2025-06-18 20:36:54 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (67.78%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,16 +1,40 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[]{-1, -1};
-
-for (int i = 0; i < nums.length; i++) {
-    if (nums[i] == target) {
-        if (res[0] == -1) {
-            res[0] = i;  // first occurrence
-        }
-        res[1] = i;      // keep updating to last occurrence
-    }
+    int[] ans = {-1, -1};
+    ans[0] = findFirst(nums, target);
+    ans[1] = findLast(nums, target);
+    return ans;
 }
-return res;
 
+public int findFirst(int[] nums, int target) {
+    int start = 0, end = nums.length - 1, res = -1;
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == target) {
+            res = mid;
+            end = mid - 1; // continue searching on left
+        } else if (nums[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
     }
+    return res;
+}
+
+public int findLast(int[] nums, int target) {
+    int start = 0, end = nums.length - 1, res = -1;
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == target) {
+            res = mid;
+            start = mid + 1; // continue searching on right
+        } else if (nums[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return res;
+}
 }
