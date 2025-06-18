9af8f8d223548ca22dfcfb4267b5c74d3c758517
Ranjit T N <ranju0305@gmail.com>
2025-06-18 21:15:56 +0530

    Time: 5 ms (25.12%) | Memory: 45.8 MB (13.23%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
