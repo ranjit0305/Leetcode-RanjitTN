71fb886bbf9d8cd199f349d3df374ed6dd491c30
Ranjit T N <ranju0305@gmail.com>
2025-06-30 21:26:31 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (38.49%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int trap(int[] height) 
+    {
+       int left=0;
+       int rightmax=0;
+       int leftmax=0;
+       int total=0;
+       int right=height.length-1;
+       while(left<right)
+       {
+            if(height[left]<=height[right])
+            {
+                if(height[left]<leftmax)
+                {
+                    total=total+(leftmax-height[left]);
+                }
+                else
+                {
+                    leftmax=height[left];
+                }
+                left=left+1;
+            }
+            else
+            {
+                if(height[right]<rightmax)
+                {
+                    total=total+(rightmax-height[right]);
+                }
+                else
+                {
+                    rightmax=height[right];
+                }
+            right=right-1;
+            }
+       } 
+        return total;
+    }  
+}
