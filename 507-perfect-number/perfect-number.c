ed16d47225cd5d050704ed588324c233d0b469a7
Ranjit T N <ranju0305@gmail.com>
2025-07-05 12:10:30 +0530

    Time: 1916 ms (5.91%) | Memory: 7.6 MB (84.49%) - LeetSync

---
+++ 507-perfect-number/perfect-number.c
@@ -0,0 +1,20 @@
+bool checkPerfectNumber(int num) {
+    int sum=0;
+    for(int i=1;i<num;i++)
+    {
+
+        if(num%i==0)
+        {
+            sum=sum+i;
+        }
+        if(sum>num)
+        {
+            return false;
+        }
+    }
+    if(sum==num)
+    {
+        return true;
+    }
+    return false;
+}
