b030878632ec129fb88a62737e1b01b44827c374
Ranjit T N <ranju0305@gmail.com>
2025-08-02 21:28:35 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (99.95%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
@@ -1,6 +1,13 @@
 class Solution {
     public String toHex(int num) {
-        String s = Integer.toHexString(num);
-        return s;
+        if (num == 0)
+            return "0";
+        char [] possibility = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        StringBuilder sb = new StringBuilder();
+        while (num != 0) {
+            sb.append(possibility[num & 15]);
+            num = (num >>> 4);
+        }
+        return sb.reverse().toString();
     }
 }
