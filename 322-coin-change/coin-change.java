316482d46531e352f0613627c7824d7e63b5d01b
Ranjit T N <ranju0305@gmail.com>
2025-08-08 23:12:27 +0530

    Time: 14 ms (87.60%) | Memory: 44.5 MB (65.70%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,16 +2,10 @@ class Solution {
         public int coinChange(int[] coins, int amount) {
         int n = amount + 1;
         int[] dp = new int[n];
-
-        // Initialize all values as a large number (infinity equivalent)
         for (int i = 0; i < n; i++) {
             dp[i] = n;
         }
-
-        // Base case: 0 amount needs 0 coins
         dp[0] = 0;
-
-        // Build up the dp array
         for (int i = 1; i <= amount; i++) {
             for (int j = 0; j < coins.length; j++) {
                 int coin = coins[j];
@@ -23,7 +17,6 @@ class Solution {
             }
         }
 
-        // If dp[amount] is still the initial large value, return -1
         if (dp[amount] == n) {
             return -1;
         } else {
