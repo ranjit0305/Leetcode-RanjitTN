6e6703843ee7d1103a0368b5224eca75066879d1
Ranjit T N <ranju0305@gmail.com>
2026-02-11 23:32:22 +0530

    Time: 2 ms (98.29%) | Memory: 48.5 MB (16.54%) - LeetSync

---
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
@@ -0,0 +1,27 @@
+class Solution {
+    List<Integer> arr = new ArrayList<>();
+
+    void inorder(TreeNode root) {
+        if (root == null) return;
+        inorder(root.left);
+        arr.add(root.val);
+        inorder(root.right);
+    }
+
+    TreeNode build(int left, int right) {
+        if (left > right) return null;
+
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(arr.get(mid));
+
+        node.left = build(left, mid - 1);
+        node.right = build(mid + 1, right);
+
+        return node;
+    }
+
+    public TreeNode balanceBST(TreeNode root) {
+        inorder(root);
+        return build(0, arr.size() - 1);
+    }
+}
